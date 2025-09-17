b98a0aceb835b91ac7969c779fd39fe06dc53168
Kendra Laing <laingk288@gmail.com>
2025-09-17 04:53:13 +0000

    edit stg_boc_rates.sql

--- models/mortgage/staging/stg_boc_rates.sql
+++ models/mortgage/staging/stg_boc_rates.sql
@@ -1,7 +1,11 @@
-{{ config(materialized='view') }}
+{{ config(
+    database='RAW',
+    schema='DBT_KLAING_MTG',
+    materialized='view'
+) }}
 
 select
-  date,
-  series,
-  value
+    date,
+    series,
+    value
 from {{ ref('boc_rates') }}
