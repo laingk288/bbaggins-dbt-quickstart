e270215d55a95db424052008b4f9b71b3568f3f9
Kendra Laing <laingk288@gmail.com>
2025-09-20 19:37:01 +0000

    created generate_schema_name.sql to prevent dbt from overwriting this macro with prefixes.

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-  {%- if custom_schema_name is none -%}
-    {{ target.schema }}         -- no custom schema specified: use target schema
-  {%- else -%}
-    {{ custom_schema_name | upper }}  -- use exactly what you set in `schema:` (no prefix)
-  {%- endif -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema | upper }}
+    {%- else -%}
+        {{ custom_schema_name | upper }}
+    {%- endif -%}
 {%- endmacro %}
